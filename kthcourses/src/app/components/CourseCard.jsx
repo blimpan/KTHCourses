29a27fd79bb170605eb0990d2e2b01065f15f730
linus <linusbalt3r@gmail.com>
2025-02-26 21:00:00 +0100

    Styling and QoL improvements.

--- kthcourses/src/app/components/CourseCard.jsx
+++ kthcourses/src/app/components/CourseCard.jsx
@@ -1,9 +1,18 @@
 
 
 export default function CourseCard(params) {
+
+    const maxDescriptionLength = 100; // number of characters
+
     return (
-        <div className="flex debug w-full h-24 bg-white shadow-md rounded-lg p-4">
-            <p>{params.title}</p>
+        <div className="flex flex-col min-w h-min bg-white drop-shadow-md rounded-lg p-4 hover:-translate-y-0.5 transition-transform ease-in-out">
+            <p className="text-lg font-medium">{params.title}</p>
+            
+            <div className="flex flex-row justify-between text-sm">
+                <p>{params.ects} ECTS</p>
+            </div>
+
+            <p className="text-sm">{params.description.length > maxDescriptionLength ? params.description.substring(0, maxDescriptionLength) + '...' : params.description}</p>
         </div>
     );    
 };
--- kthcourses/src/app/page.tsx
+++ kthcourses/src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
-import CourseCard from './components/CourseCard'
+import CourseCard from '@/app/components/CourseCard'
+import SearchPanel from '@/app/components/SearchPanel'
 import React, { useEffect, useState } from 'react'
 
 export default function Page() {
+
+  // VARIABLES
   
   const [searchText, setSearchText] = useState('');
   const [courses, setCourses] = useState<any[]>([]);
@@ -15,6 +18,8 @@ export default function Page() {
   const [readyForFetch, setReadyForFetch] = useState<boolean>(false);
 
   let debounceTimeout: NodeJS.Timeout;
+
+  // DECLARED FUNCTIONS
 
   async function fetchCourses() {
 
@@ -52,56 +57,12 @@ export default function Page() {
     // console.log(data.count)
   }
 
-  useEffect(() => {
-    if (searchText) {
-      setPageIndex(1); // Reset page to 1
-      setLoadedPages([]); // Clear loaded pages
-      setTotalCourses(0); // Reset total count
-      setCourses([]); // Clear previous courses
-      setReadyForFetch(true); // Indicate that we should fetch courses after state updates
-    }
-  }, [searchText, toggledPeriods]);
-
-  useEffect(() => {
-    if (readyForFetch) {
-      fetchCourses();
-      setReadyForFetch(false); // Reset the flag
-    }
-  }, [readyForFetch]);
-
-  useEffect(() => {
-    function handleScroll() {
-      if (debounceTimeout) {
-        clearTimeout(debounceTimeout);
-      }
-    
-      debounceTimeout = setTimeout(() => {
-        const scrollPosition = window.innerHeight + window.scrollY; // height of viewport + vertical scroll position
-        const threshold = document.documentElement.scrollHeight * 0.8;
-    
-        if (scrollPosition >= threshold) {
-          setPageIndex((prevIndex) => prevIndex + 1);
-          console.log('Page index increased');
-        }
-      }, 100);
-    }
-    
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  useEffect(() => {
-    if (pageIndex > 1) {
-      fetchCourses();
-    }
-  }, [pageIndex]);
-
   function onTextSearchChange(event: React.ChangeEvent<HTMLInputElement>) {
 
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
       setSearchText(event.target.value.trim());
-    }, 300);
+    }, 200);
   }
 
 
@@ -116,30 +77,70 @@ export default function Page() {
 
   }
 
-  return (
-   <div className='flex debug h-screen'>
-      <div className='flex flex-col debug h-screen w-min pt-4 space-y-4 p-3'> {/* Search panel */}
-        
-        <input type="text" placeholder='Search...' onChange={onTextSearchChange} className='def-border p-3 placeholder-color'/> {/* Text filter */}
-        
-        <div className='flex flex-col'> {/* Period start filter */}
-          <p>Period</p>
-          <div className='flex flex-row def-border'>
-            {['1', '2', '3', '4', 'S'].map((period) => (
-              <button
-                key={period}
-                value={period}
-                onClick={onTogglePeriod}
-                className={`px-7 py-3 border-l border-gray-500 hover:bg-gray-300 ${toggledPeriods.includes(period) ? 'bg-gray-300' : ''}`}
-              >
-                <b>{period}</b>
-              </button>
-            ))}
-          </div>
-        </div>
-      </div>
+  // EFFECTS
 
-      <div className='flex flex-col debug h-screen w-full pt-4 space-y-4 p-3'> {/* Course list */}
+  useEffect(() => { // Fetch courses when search text changes or toggled periods change. Resets variables to ensure a fresh fetch can be made.
+    if (searchText) {
+      setPageIndex(1);
+      setLoadedPages([]);
+      setTotalCourses(0);
+      setCourses([]);
+      setReadyForFetch(true); // Indicate that we should fetch courses after state updates
+    }
+  }, [searchText, toggledPeriods]);
+
+  useEffect(() => { // Fetch courses when readyForFetch flag is set to true (makes sure vars such as page index are reset before entering fetchCourses)
+    if (readyForFetch) {
+      fetchCourses();
+      window.scrollTo(0, 0);
+      setReadyForFetch(false);
+    }
+  }, [readyForFetch]);
+
+  useEffect(() => { // Increases the page index when user scrolls close to the bottom of the window. Uses a debounce timeout to prevent multiple increments.
+  
+    function handleScroll() {
+
+      const scrollPosition = window.scrollY + window.innerHeight;
+      const threshold = document.documentElement.scrollHeight - (window.innerHeight * 0.3);
+
+      if (scrollPosition >= threshold) {
+        if (debounceTimeout) {
+          clearTimeout(debounceTimeout);
+        }
+  
+        debounceTimeout = setTimeout(() => {
+          setPageIndex((prevIndex) => prevIndex + 1);
+          console.log('Page index increased');
+        }, 100);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  
+  
+  useEffect(() => {  // Fetch "next page" of courses whenever page index changes
+    if (pageIndex > 1) {
+      fetchCourses();
+    }
+  }, [pageIndex]);
+
+
+  return (
+   <div className='flex h-screen'>
+
+      <SearchPanel 
+        onTextSearchChange={onTextSearchChange}
+        onTogglePeriod={onTogglePeriod}
+        toggledPeriods={toggledPeriods}
+      />
+
+      <div className='ml-[17.3rem] flex flex-col h-full w-full pt-4 space-y-4 p-3'> {/* Course list */}
 
         {totalCourses > 0 && (
           <p>Showing {courses.length} of {totalCourses} courses</p>
@@ -152,14 +153,12 @@ export default function Page() {
 
                 <CourseCard key={course.id} 
                 title={course.course_code + " " + course.name}
+                description={course.content}
+                ects={course.ects_credits}
                 />
 
             ))}
-          
-          <div className='flex flex-row w-full h-min'>
-            <p className='text-lg font-bold'>{courses[0].course_code}</p>
-          </div>
-
+            
           </>
         ) : (
           <p>No courses found</p>
