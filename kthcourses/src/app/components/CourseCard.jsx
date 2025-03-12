524fb6af4c884ec8354213d55532cd6f00fed4c1
linus <linusbalt3r@gmail.com>
2025-03-12 18:11:52 +0100

    Removed hover from search panel toggle button and made course card appear clickable.

--- kthcourses/src/app/components/CourseCard.jsx
+++ kthcourses/src/app/components/CourseCard.jsx
@@ -5,13 +5,15 @@ export default function CourseCard(params) {
     const router = useRouter();
 
     function onCardClick() {
-        router.push(`/course/${params.code}`);       
+        if (!params.searchPanelShowing) {
+            router.push(`/course/${params.code}`);       
+        }
     }
 
     const maxDescriptionLength = 100; // number of characters
 
     return (
-        <div onClick={onCardClick} className="flex flex-col min-w h-min bg-white drop-shadow-md rounded-lg p-4 hover:-translate-y-0.5 transition-transform ease-in-out cursor-pointer">
+        <div onClick={onCardClick} className={`flex flex-col min-w h-min bg-white drop-shadow-md rounded-lg p-4 hover:-translate-y-0.5 transition-transform ease-in-out ${!params.searchPanelShowing ? 'cursor-pointer' : ''}`}>
             <p className="text-lg font-medium">{params.code + " " + params.name}</p>
             
             <div className="flex flex-row justify-between text-sm">
