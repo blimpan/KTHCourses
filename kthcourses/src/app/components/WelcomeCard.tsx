3402102801b31d9e2fcc2ebeba9d8dd25ff6faad
linus <linusbalt3r@gmail.com>
2025-04-05 16:56:08 +0200

    Added overflow-hidden to parent div.

--- kthcourses/src/app/components/WelcomeCard.tsx
+++ kthcourses/src/app/components/WelcomeCard.tsx
@@ -2,7 +2,7 @@
 export default function WelcomeCard() {
     
     return (
-        <div className="default-comp-style !p-0 flex flex-col sm:flex-row  w-full h-full bg-white">
+        <div className="default-comp-style !p-0 flex flex-col sm:flex-row  w-full h-full bg-white overflow-hidden">
             
             <div className="px-4 pt-4 sm:pb-4">
                 <h1 className="font-bold text-xl text-nowrap">Welcome to KTH Courses</h1>
@@ -10,12 +10,12 @@ export default function WelcomeCard() {
 
                 <ul className="mt-4 space-y-2 font-semibold">
                     <li className="flex items-center gap-2 text-nowrap"> <img src="/images/green-checkmark.svg" alt="Green checkmark" width={30} />2000+ courses</li>
-                    <li className="flex items-center gap-2 text-nowrap"> <img src="/images/green-checkmark.svg" alt="Green checkmark" width={30} />Easy to use search tool</li>
+                    <li className="flex items-center gap-2 text-nowrap"> <img src="/images/green-checkmark.svg" alt="Greenn checkmark" width={30} />Easy to use search tool</li>
                     <li className="flex items-center gap-2 text-nowrap"> <img src="/images/green-checkmark.svg" alt="Green checkmark" width={30} />By students, for students</li>
                 </ul>
             </div>
 
-            <div className="relative grow mt-2 overflow-clip rounded-lg h-[10rem] xs:h-[15rem] sm:h-auto w-full">
+            <div className="relative grow mt-2 overflow-hidden rounded-lg h-[10rem] xs:h-[15rem] sm:h-auto w-full">
                 <img 
                     src="/images/kth-ellipse.webp" 
                     alt="KTH Logo"
