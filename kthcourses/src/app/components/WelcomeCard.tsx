ee7eee08055592da670bb9e33d349f58217733f7
linus <linusbalt3r@gmail.com>
2025-04-07 21:38:26 +0200

    Minor styling fix

--- kthcourses/src/app/components/WelcomeCard.tsx
+++ kthcourses/src/app/components/WelcomeCard.tsx
@@ -5,8 +5,8 @@ export default function WelcomeCard() {
         <div className="default-comp-style !p-0 flex flex-col sm:flex-row  w-full h-full bg-white overflow-hidden">
             
             <div className="px-4 pt-4 sm:pb-4">
-                <h1 className="font-bold text-xl whitespace-nowrap">Welcome to KTH Courses</h1>
-                <p>An interactive catalog of all courses at KTH</p>
+                <h1 className="font-bold text-lg whitespace-nowrap">What do you want to study?</h1>
+                <p>KTH Courses is an interactive catalog of all courses at KTH</p>
 
                 <ul className="mt-4 space-y-2 font-semibold">
                     <li className="flex items-center gap-2 whitespace-nowrap"> <img src="/images/green-checkmark.svg" alt="Green checkmark" width={30} /> 2000+ courses</li>
@@ -15,11 +15,11 @@ export default function WelcomeCard() {
                 </ul>
             </div>
 
-            <div className="relative grow mt-2 overflow-hidden rounded-lg h-[10rem] xs:h-[15rem] sm:h-auto">
+            <div className="@container relative grow mt-2 overflow-hidden rounded-lg h-[10rem] xs:h-[15rem] sm:h-auto">
                 <img 
                     src="/images/kth-ellipse.webp" 
                     alt="KTH Logo"
-                    className="absolute bottom-0 right-0 object-contain object-top top-0 h-[13rem] xs:h-[18rem] sm:object-right"
+                    className="absolute bottom-0 right-0 object-contain object-top top-0 h-[13rem] hidden @[96px]:block xs:h-[18rem] sm:object-right"
                 />
             </div>
 
